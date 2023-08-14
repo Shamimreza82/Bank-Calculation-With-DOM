2364d77a75ec489d3a4cba3926acca9a89f09d05
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-15 01:55:38 +0600

    added new code

--- js/calculationWithdeaw.js
+++ js/calculationWithdeaw.js
@@ -4,13 +4,26 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawTotalValue = withdrawInput.value;
     const withdrawTotalValueString = parseFloat(withdrawTotalValue);
 
+
     const withdrawAmount = document.getElementById('withdraw-amount');
     const withdrawAmountValue = withdrawAmount.innerText;
     const withdrawAmountValueString = parseFloat(withdrawAmountValue);
     const withdrawTotal = withdrawAmountValueString + withdrawTotalValueString;
     withdrawAmount.innerText = withdrawTotal;
 
+
+
     const TotalBalance = document.getElementById('Total-balance');
     const TotalBalanceValue = TotalBalance.innerText;
+    const TotalBalanceValueString = parseFloat(TotalBalanceValue);
+    const totalBalenceWithdraw = TotalBalanceValueString - withdrawTotal;
+    TotalBalance.innerText = totalBalenceWithdraw;
+
+    withdrawInput.value = "";
+
+    if (totalBalenceWithdraw < withdrawTotalValueString) {
+        alert("You Dont have Enaguf Mony to Withdraw")
+    }
 
 })
+
