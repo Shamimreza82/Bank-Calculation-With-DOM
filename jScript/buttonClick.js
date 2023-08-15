50f3536edda786e3a75ebd36833015d39b855807
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-16 00:13:44 +0600

    total Update

--- jScript/buttonClick.js
+++ jScript/buttonClick.js
@@ -1,4 +1,22 @@
 document.getElementById('deposit-btn').addEventListener('click', function () {
+    const InputFieldTotal = inputField('deposit-input');
+    const inputTextTotal = inputTextField('display-deposit');
+    const Total = InputFieldTotal + inputTextTotal;
+    valueDisplay('display-deposit', Total);
+    const inputBalanceTextTotal = inputTextField('Total-balance');
+    const totalBalance = inputBalanceTextTotal + Total;
+    valueDisplay('Total-balance', totalBalance);
+})
 
+
+
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawInputFieldTotal = inputField('withdraw-input');
+    const withdrawInputTextTotal = inputTextField('withdraw-amount');
+    const withdrawTotal = withdrawInputFieldTotal + withdrawInputTextTotal;
+    valueDisplay('withdraw-amount', withdrawTotal);
+    const inputBalanceTextTotal = inputTextField('Total-balance');
+    const withdrawTotalBalance = inputBalanceTextTotal - withdrawInputFieldTotal;
+    valueDisplay('Total-balance', withdrawTotalBalance);
 
 })
--- jScript/utilitis.js
+++ jScript/utilitis.js
@@ -1,4 +1,21 @@
+
 function inputField(inputId) {
     const inputField = document.getElementById(inputId);
     const inputFieldString = inputField.value;
+    const inputFieldConvert = parseFloat(inputFieldString);
+    inputField.value = "";
+    return inputFieldConvert;
 }
+
+function inputTextField(inputId) {
+    const inputTextField = document.getElementById(inputId);
+    const inputTextFieldString = inputTextField.innerText;
+    const inputTextFieldConvert = parseFloat(inputTextFieldString);
+    return inputTextFieldConvert;
+}
+
+function valueDisplay(inputId, total) {
+    const valueDisplay = document.getElementById(inputId);
+    const valueDisplayIn = valueDisplay.innerText;
+    valueDisplay.innerText = total;
+} 
